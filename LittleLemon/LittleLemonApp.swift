062d9f477942e232e18ecaabc004d1253fbd9f75
Edwin Pérez <edwin@edarpe.com>
2024-08-12 08:59:05 -0600

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Edwin Perez on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
